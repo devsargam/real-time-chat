17b18b9bb2f621989ae0d9cf789f54d7b45cda6c
devsargam <sargampoudel100@gmail.com>
2024-01-10 14:28:51 +0530

    fix(backend): setup `io`

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import http from 'http';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = +process.env.PORT! ?? 3000;
 
 app.get('/', (_, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, () => {
+io.on('connection', () => {
+  console.log('someone connected to this chat');
+});
+
+server.listen(PORT, () => {
   console.info(`Server running on port ${PORT}`);
 });
