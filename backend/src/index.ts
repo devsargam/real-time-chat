5dc36dffa259b4cdc4f5fde60b7fa2be0b4fec90
devsargam <sargampoudel100@gmail.com>
2024-01-11 18:23:47 +0530

    fix(backend): add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,11 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 const PORT = +process.env.PORT! ?? 3000;
 
@@ -28,6 +32,7 @@ app.get('/', (_, res) => {
 });
 
 io.on('connection', (socket) => {
+  socket.send('connection sucessful');
   socket.on('event', (data) => {
     console.log('event here');
     console.log(data);
