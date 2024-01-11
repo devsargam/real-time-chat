cfc40c971f34ac07c8ea9e751e62384f6df496a8
devsargam <sargampoudel100@gmail.com>
2024-01-11 18:23:21 +0530

    feat(io): setup socketio in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,44 @@
-import Homepage from "./Components/Homepage/Homepage";
+import { useEffect, useState } from 'react';
+import { socket } from './socket';
+import Homepage from './Components/Homepage/Homepage';
 
 const App = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents((previous) => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('message', (data) => {
+      setFooEvents((prev) => [...prev, data]);
+    });
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
   return (
     <>
       {/* <Sidebar /> */}
+      {JSON.stringify({
+        isConnected,
+        fooEvents,
+      })}
       <Homepage />
     </>
   );
