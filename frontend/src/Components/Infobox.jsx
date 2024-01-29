dfddcf7e08713a7cb2342da4db4567fceada8b73
devsargam <sargampoudel100@gmail.com>
2024-01-29 23:53:39 +0530

    feat: setup prop types and implement in infobox

--- frontend/src/Components/Infobox.jsx
+++ frontend/src/Components/Infobox.jsx
@@ -1,4 +1,7 @@
-const Infobox = () => {
+import PropTypes from 'prop-types';
+
+const Infobox = ({ username = 'Default' }) => {
+  console.log(username);
   return (
     <div>
       <div className="container flex lg:gap-[7vw] md:gap[15vw]] gap-10 items-center py-2 px-2 border-2 rounded-md border-solid border-gray-500 w-max">
@@ -7,10 +10,15 @@ const Infobox = () => {
           className="w-11 h-11 rounded-full"
           loading="lazy"
         />
-        <p className="text-slate-600">Sargam</p>
+        <p className="text-slate-600">{username}</p>
       </div>
     </div>
   );
+};
+
+// This adds props validation to jsx maybe
+Infobox.propTypes = {
+  username: PropTypes.string.isRequired,
 };
 
 export default Infobox;
